6f22c2bf18993a147d5323330b42e641dc2c662f
Felipe Silva <felipe.allware@gmail.com>
2025-02-10 20:55:03 -0300

    refactor: Adicionando um enum a model de mouse event

---
+++ server/enums/Direction.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Direction:
+    CENTER = "center"
+    UP = "up"
+    DOWN = "down"
+    LEFT = "left"
+    RIGHT = "right"
--- server/events/mouse_events.py
+++ server/events/mouse_events.py
@@ -1,6 +1,7 @@
 from flask_socketio import SocketIO
 from models.mouse_event import MouseEvent
 from services.mouse_service import MouseService
+from enums.Direction import Direction
 
 def register_mouse_events(socketio: SocketIO):
     @socketio.on("move_mouse")
@@ -8,7 +9,7 @@ def register_mouse_events(socketio: SocketIO):
         event = MouseEvent(**data)
 
         match event.direction:
-            case "center":
+            case Direction.CENTER:
                 MouseService.move_mouse_center()
             case _:
                 print(f"Invalid direction: {event.direction}")
